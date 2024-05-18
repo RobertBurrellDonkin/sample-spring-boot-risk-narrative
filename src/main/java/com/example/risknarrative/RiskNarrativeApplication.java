2b255079b96ff2a8bd8906c213e35aeb04cfd94f
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2024-05-18 19:26:02 +0100

    Generated Spring Boot project

---
+++ src/main/java/com/example/risknarrative/RiskNarrativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.risknarrative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskNarrativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskNarrativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/risknarrative/RiskNarrativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.risknarrative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskNarrativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
