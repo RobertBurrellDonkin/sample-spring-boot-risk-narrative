06804f8d1d186027b0bafe3603f937d116d9345b
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2024-05-19 14:02:10 +0100

    Integration test when is passed in.

--- src/test/java/com/example/risknarrative/api/company/search/EndToEndTest.java
+++ src/test/java/com/example/risknarrative/api/company/search/EndToEndTest.java
@@ -1,6 +1,9 @@
 package com.example.risknarrative.api.company.search;
 
+import com.example.risknarrative.services.company.CompanyService;
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
 import org.junit.jupiter.api.*;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Fail.fail;
 import static org.springframework.cloud.contract.wiremock.WireMockSpring.options;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -22,8 +26,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class EndToEndTest {
 
+
+    public static final String TRU_PROXY_API = "/TruProxyAPI/rest/Companies/v1/";
+    public static final String TRU_PROXY_SEARCH_PATH = TRU_PROXY_API + "Search";
+    public static final String TRU_PROXY_OFFICERS_PATH = TRU_PROXY_API + "Officers";
+    public static final String X_API_KEY = "x-api-key";
+
     public static final String COMPANY_SEARCH_URL = "/api/company/search";
     public static WireMockServer wiremock = new WireMockServer(options().dynamicPort());
+
+    @Autowired
+    CompanyService companyService;
 
     @BeforeAll
     static void setupClass() {
@@ -40,9 +53,100 @@ public class EndToEndTest {
         wiremock.shutdown();
     }
 
+
+    @BeforeEach
+    void setUpMocks() {
+        companyService.setTruProxyBaseUrl(wiremock.baseUrl());
+    }
+
+
     @Test
-    @Disabled
     void searchWithCompanyNumberAndCompanyName(@Autowired MockMvc mvc) throws Exception {
+        stubFor(get(urlPathEqualTo(TRU_PROXY_SEARCH_PATH)).willReturn(
+                aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                                """
+                                         {
+                                           "page_number": 1,
+                                           "kind": "search#companies",
+                                           "total_results": 20,
+                                           "items": [
+                                               {
+                                                   "company_status": "active",
+                                                   "address_snippet": "Boswell Cottage Main Street, North Leverton, Retford, England, DN22 0AD",
+                                                   "date_of_creation": "2008-02-11",
+                                                   "matches": {
+                                                       "title": [
+                                                           1,
+                                                           3
+                                                       ]
+                                                   },
+                                                   "description": "06500244 - Incorporated on 11 February 2008",
+                                                   "links": {
+                                                       "self": "/company/06500244"
+                                                   },
+                                                   "company_number": "06500244",
+                                                   "title": "BBC LIMITED",
+                                                   "company_type": "ltd",
+                                                   "address": {
+                                                       "premises": "Boswell Cottage Main Street",
+                                                       "postal_code": "DN22 0AD",
+                                                       "country": "England",
+                                                       "locality": "Retford",
+                                                       "address_line_1": "North Leverton"
+                                                   },
+                                                   "kind": "searchresults#company",
+                                                   "description_identifier": [
+                                                       "incorporated-on"
+                                                   ]
+                                               }]
+                                         }
+                                        """
+                        )));
+
+        stubFor(get(urlPathEqualTo(TRU_PROXY_OFFICERS_PATH)).willReturn(
+                aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                                """
+                                        {
+                                            "etag": "6dd2261e61776d79c2c50685145fac364e75e24e",
+                                            "links": {
+                                                "self": "/company/10241297/officers"
+                                            },
+                                            "kind": "officer-list",
+                                            "items_per_page": 35,
+                                            "items": [
+                                                {
+                                                    "address": {
+                                                        "premises": "The Leeming Building",
+                                                        "postal_code": "LS2 7JF",
+                                                        "country": "England",
+                                                        "locality": "Leeds",
+                                                        "address_line_1": "Vicar Lane"
+                                                    },
+                                                    "name": "ANTLES, Kerri",
+                                                    "appointed_on": "2017-04-01",
+                                                    "resigned_on": "2018-02-12",
+                                                    "officer_role": "director",
+                                                    "links": {
+                                                        "officer": {
+                                                            "appointments": "/officers/4R8_9bZ44w0_cRlrxoC-wRwaMiE/appointments"
+                                                        }
+                                                    },
+                                                    "date_of_birth": {
+                                                        "month": 6,
+                                                        "year": 1969
+                                                    },
+                                                    "occupation": "Finance And Accounting",
+                                                    "country_of_residence": "United States",
+                                                    "nationality": "American"
+                                                }]
+                                          }
+                                """
+                        )));
+
         var expectedSearchResults = """
                 {
                     "total_results": 1,
@@ -62,15 +166,15 @@ public class EndToEndTest {
                             },
                             "officers": [
                                 {
-                                    "name": "BOXALL, Sarah Victoria",
-                                    "officer_role": "secretary",
-                                    "appointed_on": "2008-02-11",
+                                    "name": "ANTLES, Kerri",
+                                    "officer_role": "director",
+                                    "appointed_on": "2017-04-01",
                                     "address": {
-                                        "premises": "5",
-                                        "locality": "London",
-                                        "address_line_1": "Cranford Close",
+                                        "premises": "The Leeming Building",
+                                        "locality": "Leeds",
+                                        "address_line_1": "Vicar Lane",
                                         "country": "England",
-                                        "postal_code": "SW20 0DP"
+                                        "postal_code": "LS2 7JF"
                                     }
                                 }
                             ]
@@ -81,6 +185,7 @@ public class EndToEndTest {
         var actualSearchResults = mvc.perform(
                 post(COMPANY_SEARCH_URL)
                         .contentType(APPLICATION_JSON) //TODO: Other Content Types
+                        .header(X_API_KEY, "some-api-key")
                         .content("""
                                 {
                                     "companyName" : "BBC LIMITED",
@@ -142,5 +247,14 @@ public class EndToEndTest {
 
     // TODO: Check Sanitise Query String
     // Should be a POST, not a GET
+
+
+    private static void verify(RequestPatternBuilder requestedFor) {
+        wiremock.verify(requestedFor);
+    }
+
+    private void stubFor(MappingBuilder mappingBuilder) {
+        wiremock.stubFor(mappingBuilder);
+    }
 }
 
